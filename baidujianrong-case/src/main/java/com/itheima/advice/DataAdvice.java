58621be69bf6d0e70af2524ec9964df62f8b3d59
daima-883 <2563663078@qq.com>
2022-09-22 12:01:31 +0800

    百度兼容初始化完成，并未更改。

---
+++ baidujianrong-case/src/main/java/com/itheima/advice/DataAdvice.java
@@ -0,0 +1,21 @@
+package com.itheima.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class DataAdvice {
+    @Pointcut("execution(boolean com.itheima.service.*Service.*(*,*))")
+    private void servicePt(){}
+
+    @Around("DataAdvice.servicePt()")
+    public Object trimStr(ProceedingJoinPoint pjp) throws Throwable {
+
+        Object ret = pjp.proceed();
+        return ret;
+    }
+}
