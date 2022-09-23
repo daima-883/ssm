995963ac116d7aff7d92e6c15f67fa7fea8b0257
daima-883 <2563663078@qq.com>
2022-09-23 10:48:50 +0800

    springmvc入门问题在于用tomcat插件可以运行但是本地的tomcat不行

---
+++ spring-mvc/src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+    @RequestMapping("/save")//设置映射路径为/save，即外部访问路径
+    @ResponseBody//设置当前操作返回结果为指定json数据（本质上是一个字符串信息）
+    public String save(){
+        System.out.println("user save ...");
+        return "{'info':'springmvc'}";
+    }
+}
