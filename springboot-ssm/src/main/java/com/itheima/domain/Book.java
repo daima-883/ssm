187006ad63e560582904509d0af7e39da2b54651
daima-883 <2563663078@qq.com>
2022-10-27 15:28:16 +0800

    MP映射匹配兼容性

--- springboot-ssm/src/main/java/com/itheima/domain/Book.java
+++ springboot-ssm/src/main/java/com/itheima/domain/Book.java
@@ -1,8 +1,10 @@
 package com.itheima.domain;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("tbl_book")
 public class Book {
     private Integer id;
     private String name;
