9556664a93e1b8fd1e6a6d0afbe9052666369dd9
daima-883 <2563663078@qq.com>
2022-10-27 15:50:58 +0800

    MP id生成策略

--- springboot-ssm/src/main/java/com/itheima/domain/Book.java
+++ springboot-ssm/src/main/java/com/itheima/domain/Book.java
@@ -1,10 +1,11 @@
 package com.itheima.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
-@TableName("tbl_book")
 public class Book {
     private Integer id;
     private String name;
