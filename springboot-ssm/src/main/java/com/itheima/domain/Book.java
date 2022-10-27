bd55bca29d0503a42b7836f385b5e7fa93b3089e
daima-883 <2563663078@qq.com>
2022-10-27 16:27:55 +0800

    MP 逻辑删除

--- springboot-ssm/src/main/java/com/itheima/domain/Book.java
+++ springboot-ssm/src/main/java/com/itheima/domain/Book.java
@@ -2,6 +2,7 @@ package com.itheima.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -12,4 +13,6 @@ public class Book {
     private String type;
     private String description;
     private Integer count;
+    @TableLogic(value = "0",delval = "1")
+    private Integer deleted;
 }
--- springboot-ssm/src/test/java/com/itheima/SpringbootSsmApplicationTests.java
+++ springboot-ssm/src/test/java/com/itheima/SpringbootSsmApplicationTests.java
@@ -1,32 +1,17 @@
 package com.itheima;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.itheima.domain.Book;
-import com.itheima.domain.QueryBook;
 import com.itheima.mapper.BookMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 @SpringBootTest
 class SpringbootSsmApplicationTests {
     @Autowired
     private BookMapper bookMapper;
-    //测试批量删除，查询同理
+    //逻辑删除，数据库实际没有删除，只是加了标记
     @Test
     public void testDelete(){
-        List<Integer> bookList = new ArrayList<>();
-        bookList.add(7);
-        bookList.add(8);
-        bookList.add(9);
-        bookMapper.deleteBatchIds(bookList);
-
+        bookMapper.deleteById(14);
     }
 }
